eff28456a912ff1d2090d9d5f43a567f98626e22
beaJenius <beajenius@yandex.ru>
2022-05-05 01:22:57 +0300

    init: create keys_en.js, add keysEn object with keys descriptions

---
+++ VKeyboard/js/keys_en.js
@@ -0,0 +1,248 @@
+const keysEn = { 
+  'Digit1' : {
+    value: '1',
+    shiftValue: '!',
+  }, 
+  'Digit2' : {
+    value: '2',
+    shiftValue: '@',
+  }, 
+  'Digit3' : {
+    value: '3',
+    shiftValue: '#',
+  }, 
+  'Digit4' :  { 
+    value: '4',
+    shiftValue: '$',
+  }, 
+  'Digit5': {
+    value: '5',
+    shiftValue: '%',
+  },
+  'Digit6': {
+    value: '6',
+    shiftValue: '^',
+  },
+  'Digit7': {
+    value: '7',
+    shiftValue: '&',
+  },
+  'Digit8': {
+    value: '8',
+    shiftValue: '*',
+  },
+  'Digit9': {
+    value: '9',
+    shiftValue: '(',
+  },
+  'Digit0': {
+    value: '0',
+    shiftValue: ')',
+  },
+  'KeyA': {
+    value: 'a',
+    shiftValue: 'A',
+  },
+  'KeyB': {
+    value: 'b',
+    shiftValue: 'B',
+  },
+  'KeyC': {
+    value: 'c',
+    shiftValue: 'C',
+  },
+  'KeyD': {
+    value: 'd',
+    shiftValue: 'D',
+  },
+  'KeyE': {
+    value: 'e',
+    shiftValue: 'E',
+  },
+  'KeyF': {
+    value: 'f',
+    shiftValue: 'F',
+  },
+  'KeyG': {
+    value: 'g',
+    shiftValue: 'G',
+  },
+  'KeyJ': {
+    value: 'j',
+    shiftValue: 'J',
+  },
+  'KeyH': {
+    value: 'h',
+    shiftValue: 'H',
+  },
+  'KeyK': {
+    value: 'k',
+    shiftValue: 'K',
+  },
+  'KeyL': {
+    value: 'l',
+    shiftValue: 'L',
+  },
+  'KeyM': {
+    value: 'm',
+    shiftValue: 'M',
+  },
+  'KeyI': {
+    value: 'i',
+    shiftValue: 'I',
+  },
+  'KeyU': {
+    value: 'u',
+    shiftValue: 'U',
+  },
+  'KeyS': {
+    value: 's',
+    shiftValue: 'S',
+  },
+  'KeyN': {
+    value: 'n',
+    shiftValue: 'N',
+  },
+  'KeyO': {
+    value: 'o',
+    shiftValue: 'O',
+  },
+  'KeyP': {
+    value: 'p',
+    shiftValue: 'P',
+  },
+  'KeyV': {
+    value: 'v',
+    shiftValue: 'V',
+  },
+  'KeyT': {
+    value: 't',
+    shiftValue: 'T',
+  },
+  'KeyR': {
+    value: 'r',
+    shiftValue: 'R',
+  },
+  'KeyW': {
+    value: 'w',
+    shiftValue: 'W',
+  },
+  'KeyQ': {
+    value: 'q',
+    shiftValue: 'Q',
+  },
+  'KeyX': {
+    value: 'x',
+    shiftValue: 'X',
+  },
+  'KeyY': {
+    value: 'y',
+    shiftValue: 'Y',
+  },
+  'KeyZ': {
+    value: 'z',
+    shiftValue: 'Z',
+  },
+  'Backquote': {
+    value: '`',
+    shiftValue: '~',
+  },
+  'Minus': {
+    value: '-',
+    shiftValue: '_',
+  },
+  'Equal': {
+    value: '=',
+    shiftValue: '+',
+  },
+  'Backslash': {
+    value: '\x2F',
+    shiftValue: '|',
+  },
+  'Backspace': {
+    value: '',
+    shiftValue: '',
+  },
+  'Tab': {
+    value: '',
+    shiftValue: '',
+  },
+  'BracketLeft': {
+    value: '[',
+    shiftValue: '{',
+  },
+  'BracketRight': {
+    value: ']',
+    shiftValue: '}',
+  },
+  'Enter': {
+    value: '',
+    shiftValue: '',
+  },
+  'CapsLock': {
+    value: '',
+    shiftValue: '',
+  },
+  'Semicolon': {
+    value: ';',
+    shiftValue: ':',
+  },
+  'ShiftLeft': {
+    value: '',
+    shiftValue: '',
+  },
+  'Comma': {
+    value: ',',
+    shiftValue: '<',
+  },
+  'Period': {
+    value: '.',
+    shiftValue: '>',
+  },
+  'Slash': {
+    value: '/',
+    shiftValue: '?',
+  },
+  'ShiftRight': {
+    value: '',
+    shiftValue: '',
+  },
+  'ControlLeft': {
+    value: '',
+    shiftValue: '',
+  },
+  'MetaLeft': {
+    value: '',
+    shiftValue: '',
+  },
+  'AltLeft': {
+    value: '',
+    shiftValue: '',
+  },
+  'Space': {
+    value: ' ',
+    shiftValue: ' ',
+  },
+  'AltRight': {
+    value: '',
+    shiftValue: '',
+  },
+  'MetaRight': {
+    value: '',
+    shiftValue: '',
+  },
+  'ContextMenu': {
+    value: '',
+    shiftValue: '',
+  },
+  'ControlRight': {
+    value: '',
+    shiftValue: '',
+  },
+  'Quote': {
+    value: '\'',
+    shiftValue: '"',
+  },
+};
+
+export default keysEn;
