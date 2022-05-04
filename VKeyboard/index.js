84f0736380292a48f61cb5f7b1ecd12dbd554a4d
beaJenius <beajenius@yandex.ru>
2022-05-05 01:20:23 +0300

    init: create main JS module, create TaskApp instance

---
+++ VKeyboard/index.js
@@ -0,0 +1,14 @@
+import TaskApp from './js/app.js';
+
+const myApp = new TaskApp();
+myApp.init();
+// const log = document.getElementById('log');
+
+// const output = document.getElementById('output');
+
+/* function logKey(e) {
+  // output.innerText += `${e.code}`;
+  console.log(`${e.code}`);
+} */
+
+// document.addEventListener('keydown', logKey);
