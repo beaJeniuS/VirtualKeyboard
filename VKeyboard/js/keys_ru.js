c201297c5d42efcff54502f73dd2e8de69175986
beaJenius <beajenius@yandex.ru>
2022-05-05 01:23:20 +0300

    init: create keys_ru.js, add keysRu object with keys descriptions

---
+++ VKeyboard/js/keys_ru.js
@@ -0,0 +1,248 @@
+const keysRu = { 
+  'Digit1' : {
+    value: '1',
+    shiftValue: '!',
+  }, 
+  'Digit2' : {
+    value: '2',
+    shiftValue: '"',
+  }, 
+  'Digit3' : {
+    value: '3',
+    shiftValue: '№',
+  }, 
+  'Digit4' :  { 
+    value: '4',
+    shiftValue: ';',
+  }, 
+  'Digit5': {
+    value: '5',
+    shiftValue: '%',
+  },
+  'Digit6': {
+    value: '6',
+    shiftValue: ':',
+  },
+  'Digit7': {
+    value: '7',
+    shiftValue: '?',
+  },
+  'Digit8': {
+    value: '8',
+    shiftValue: '*',
+  },
+  'Digit9': {
+    value: '9',
+    shiftValue: '(',
+  },
+  'Digit0': {
+    value: '0',
+    shiftValue: ')',
+  },
+  'KeyA': {
+    value: 'ф',
+    shiftValue: 'Ф',
+  },
+  'KeyB': {
+    value: 'и',
+    shiftValue: 'И',
+  },
+  'KeyC': {
+    value: 'с',
+    shiftValue: 'С',
+  },
+  'KeyD': {
+    value: 'в',
+    shiftValue: 'В',
+  },
+  'KeyE': {
+    value: 'у',
+    shiftValue: 'У',
+  },
+  'KeyF': {
+    value: 'а',
+    shiftValue: 'А',
+  },
+  'KeyG': {
+    value: 'п',
+    shiftValue: 'П',
+  },
+  'KeyJ': {
+    value: 'о',
+    shiftValue: 'О',
+  },
+  'KeyH': {
+    value: 'р',
+    shiftValue: 'Р',
+  },
+  'KeyK': {
+    value: 'л',
+    shiftValue: 'Л',
+  },
+  'KeyL': {
+    value: 'д',
+    shiftValue: 'Д',
+  },
+  'KeyM': {
+    value: 'ь',
+    shiftValue: 'Ь',
+  },
+  'KeyI': {
+    value: 'ш',
+    shiftValue: 'Ш',
+  },
+  'KeyU': {
+    value: 'г',
+    shiftValue: 'Г',
+  },
+  'KeyS': {
+    value: 'ы',
+    shiftValue: 'Ы',
+  },
+  'KeyN': {
+    value: 'т',
+    shiftValue: 'Т',
+  },
+  'KeyO': {
+    value: 'щ',
+    shiftValue: 'Щ',
+  },
+  'KeyP': {
+    value: 'з',
+    shiftValue: 'З',
+  },
+  'KeyV': {
+    value: 'м',
+    shiftValue: 'М',
+  },
+  'KeyT': {
+    value: 'е',
+    shiftValue: 'Е',
+  },
+  'KeyR': {
+    value: 'к',
+    shiftValue: 'К',
+  },
+  'KeyW': {
+    value: 'ц',
+    shiftValue: 'W',
+  },
+  'KeyQ': {
+    value: 'й',
+    shiftValue: 'Й',
+  },
+  'KeyX': {
+    value: 'ч',
+    shiftValue: 'Ч',
+  },
+  'KeyY': {
+    value: 'н',
+    shiftValue: 'Н',
+  },
+  'KeyZ': {
+    value: 'я',
+    shiftValue: 'Я',
+  },
+  'Backquote': {
+    value: 'ё',
+    shiftValue: 'Ё',
+  },
+  'Minus': {
+    value: '-',
+    shiftValue: '_',
+  },
+  'Equal': {
+    value: '=',
+    shiftValue: '+',
+  },
+  'Backslash': {
+    value: '\x2F',
+    shiftValue: '/',
+  },
+  'Backspace': {
+    value: '',
+    shiftValue: '',
+  },
+  'Tab': {
+    value: '',
+    shiftValue: '',
+  },
+  'BracketLeft': {
+    value: 'х',
+    shiftValue: 'Х',
+  },
+  'BracketRight': {
+    value: 'ъ',
+    shiftValue: 'Ъ',
+  },
+  'Enter': {
+    value: '',
+    shiftValue: '',
+  },
+  'CapsLock': {
+    value: '',
+    shiftValue: '',
+  },
+  'Semicolon': {
+    value: ';',
+    shiftValue: ':',
+  },
+  'ShiftLeft': {
+    value: '',
+    shiftValue: '',
+  },
+  'Comma': {
+    value: 'б',
+    shiftValue: 'Б',
+  },
+  'Period': {
+    value: 'ю',
+    shiftValue: 'Ю',
+  },
+  'Slash': {
+    value: '.',
+    shiftValue: ',',
+  },
+  'ShiftRight': {
+    value: '',
+    shiftValue: '',
+  },
+  'ControlLeft': {
+    value: '',
+    shiftValue: '',
+  },
+  'MetaLeft': {
+    value: '',
+    shiftValue: '',
+  },
+  'AltLeft': {
+    value: '',
+    shiftValue: '',
+  },
+  'Space': {
+    value: ' ',
+    shiftValue: ' ',
+  },
+  'AltRight': {
+    value: '',
+    shiftValue: '',
+  },
+  'MetaRight': {
+    value: '',
+    shiftValue: '',
+  },
+  'ContextMenu': {
+    value: '',
+    shiftValue: '',
+  },
+  'ControlRight': {
+    value: '',
+    shiftValue: '',
+  },
+  'Quote': {
+    value: 'э',
+    shiftValue: 'Э',
+  },
+};
+
+export default keysRu;
