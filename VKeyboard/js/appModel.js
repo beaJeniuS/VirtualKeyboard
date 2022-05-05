5794fafdcadcb55a7bc3d73ebe9983558daa7a79
beaJenius <beajenius@yandex.ru>
2022-05-05 23:54:31 +0300

    feat: modify page model

--- VKeyboard/js/appModel.js
+++ VKeyboard/js/appModel.js
@@ -1,8 +1,8 @@
-import keysEn from './keys_en.js'
-import keysRu from './keys_ru.js'
-//import getCurrentLang from './app.js'
+import keysEn from './keys_en.js';
+import keysRu from './keys_ru.js';
+// import getCurrentLang from './app.js'
 
-//let currentLang = getCurrentLang()
+// let currentLang = getCurrentLang()
 
 const headerModel = {
   type: 'header',
@@ -21,176 +21,191 @@ const headerModel = {
           children: [
             {
               type: 'h1',
-              content: 'Виртуальная клавиатура',
+              content: 'RSS Виртуальная клавиатура',
               classes: ['header__title'],
-              children: []
-            }									
-          ]
-        }
-      ]
-    }
-  ]
+              children: [],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
+
+function returnButtonsLine(arr = []) {
+  const resultArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    resultArr.push(
+      {
+        type: 'div',
+        content: '',
+        classes: ['content__button', 'button'],
+        styles: [],
+        action: true,
+        children: [
+          {
+            type: 'div',
+            content: '',
+            classes: ['button__lang-en'],
+            children: [
+              {
+                type: 'span',
+                content: keysEn[arr[i]].value,
+                classes: ['button__value-CaseDown'],
+                children: [],
+              },
+              {
+                type: 'span',
+                content: keysEn[arr[i]].shiftValue,
+                classes: ['button__value-CaseUp', 'hidden'],
+                children: [],
+              },
+              {
+                type: 'span',
+                content: keysEn[arr[i]].shiftValue,
+                classes: ['button__value-Caps', 'hidden'],
+                children: [],
+              },
+              {
+                type: 'span',
+                content: keysEn[arr[i]].value,
+                classes: ['button__value-ShiftCaps', 'hidden'],
+                children: [],
+              },
+            ],
+          },
+          {
+            type: 'div',
+            content: '',
+            classes: ['button__lang-ru', 'hidden'],
+            children: [
+              {
+                type: 'span',
+                content: keysRu[arr[i]].value,
+                classes: ['button__value-CaseDown'],
+                children: [],
+              },
+              {
+                type: 'span',
+                content: keysRu[arr[i]].shiftValue,
+                classes: ['button__value-CaseUp', 'hidden'],
+                children: [],
+              },
+              {
+                type: 'span',
+                content: keysRu[arr[i]].shiftValue,
+                classes: ['button__value-Caps', 'hidden'],
+                children: [],
+              },
+              {
+                type: 'span',
+                content: keysRu[arr[i]].value,
+                classes: ['button__value-ShiftCaps', 'hidden'],
+                children: [],
+              },
+            ],
+          },
+        ],
+      },
+    );
+  }
+  return resultArr;
 }
 
-function returnButtonsLine(arr = []){
-  let resultArr = []
-  for(let i = 0;i < arr.length;i++) {
-    resultArr.push(
+const buttonsLine1 = returnButtonsLine(['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace']);
+buttonsLine1[buttonsLine1.length - 1].classes.push('button-double-size');
+buttonsLine1[buttonsLine1.length - 1].classes.push('button-dark');
+
+const buttonsLine2 = returnButtonsLine(['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash', 'Del']);
+buttonsLine2[0].classes.push('button-15-size');
+buttonsLine2[0].classes.push('button-dark');
+buttonsLine2[buttonsLine2.length - 1].classes.push('button-dark');
+
+const buttonsLine3 = returnButtonsLine(['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter']);
+buttonsLine3[0].classes.push('button-double-size');
+buttonsLine3[0].classes.push('button-dark');
+buttonsLine3[buttonsLine3.length - 1].classes.push('button-double-size');
+buttonsLine3[buttonsLine3.length - 1].classes.push('button-dark');
+
+const buttonsLine4 = returnButtonsLine(['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight']);
+buttonsLine4[0].classes.push('button-double-size');
+buttonsLine4[0].classes.push('button-dark');
+buttonsLine4[buttonsLine4.length - 1].classes.push('button-double-size');
+buttonsLine4[buttonsLine4.length - 1].classes.push('button-dark');
+
+const buttonsLine5 = returnButtonsLine(['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ArrowLeft', 'ArrowDown', 'ArrowRight', 'ControlRight']);
+for (let i = 0; i < buttonsLine5.length; i += 1) {
+  if (i !== 3) {
+    buttonsLine5[i].classes.push('button-dark');
+  } else {
+    buttonsLine5[i].classes.push('button-space');
+  }
+}
+
+const contentModel = {
+  type: 'main',
+  content: '',
+  classes: ['content'],
+  children: [
     {
       type: 'div',
       content: '',
-      classes: ['content__button', 'button'],
-      styles: [],
-      action: true, //  pointer-events!!!!!!!!!!?????????
+      classes: ['container'],
       children: [
         {
           type: 'div',
           content: '',
-          classes: ['button__lang-en'],
+          classes: ['content__column'],
           children: [
             {
-              type: 'span',
-              content:keysEn[arr[i]].shiftValue,
-              classes: ['button__value-CaseDown'],
+              type: 'textarea',
+              content: '',
+              classes: ['content__inputarea'],
               children: [],
             },
             {
-              type: 'span',
-              content: keysEn[arr[i]].shiftValue,
-              classes: ['button__value-CaseUp', 'hidden'],
-              children: []
+              type: 'div',
+              content: '',
+              classes: ['content__keyboard-container'],
+              children: [
+                {
+                  type: 'div',
+                  content: '',
+                  classes: ['content__keyboard-row'],
+                  children: buttonsLine1,
+                },
+                {
+                  type: 'div',
+                  content: '',
+                  classes: ['content__keyboard-row'],
+                  children: buttonsLine2,
+                },
+                {
+                  type: 'div',
+                  content: '',
+                  classes: ['content__keyboard-row'],
+                  children: buttonsLine3,
+                },
+                {
+                  type: 'div',
+                  content: '',
+                  classes: ['content__keyboard-row'],
+                  children: buttonsLine4,
+                },
+                {
+                  type: 'div',
+                  content: '',
+                  classes: ['content__keyboard-row'],
+                  children: buttonsLine5,
+                },
+              ],
             },
-            {
-              type: 'span',
-              content: keysEn[arr[i]].shiftValue,
-              classes: ['button__value-Caps', 'hidden'],
-              children: []
-            },	
-            {
-              type: 'span',
-              content: keysEn[arr[i]].value,
-              classes: ['button__value-ShiftCaps', 'hidden'],
-              children: []
-            },	
-          ]
-        },					
-        {
-          type: 'div',
-          content: '',
-          classes: ['button__lang-ru', 'hidden'],
-          children: [
-            {
-              type: 'span',
-              content: arr[i], // взять из объекта с описанием кнопок
-              classes: ['button__value-normal', 'hidden'],
-              children: []
-            },
-            {
-              type: 'span',
-              content: arr[i], // взять из объекта с описанием кнопок
-              classes: ['button__value-shift', 'hidden'],
-              children: []
-            },
-            {
-              type: 'span',
-              content: arr[i], // взять из объекта с описанием кнопок
-              classes: ['button__value-caps', 'hidden'],
-              children: []
-            },	
-          ]
-        }
-      ]
-    });
-  }
-  return resultArr
-}
-
-let buttonsLine1 = returnButtonsLine(['Backquote','Digit1','Digit2','Digit3','Digit4','Digit5','Digit6','Digit7','Digit8','Digit9','Digit0','Minus','Equal','Backspace']);
-buttonsLine1[buttonsLine1.length-1].classes.push('button-double-size');
-buttonsLine1[buttonsLine1.length-1].classes.push('button-dark');
-
-let buttonsLine2 = returnButtonsLine(['Tab','q','w','e','r','t','y','u','i','o','p','[',']','\x5c','Del']);
-buttonsLine2[0].classes.push('button-15-size');
-buttonsLine2[0].classes.push('button-dark');
-buttonsLine2[buttonsLine2.length-1].classes.push('button-dark');
-
-let buttonsLine3 = returnButtonsLine(['CapsLock','a','s','d','f','g','h','j','k','l',';','\'','Enter']);
-buttonsLine3[0].classes.push('button-double-size');
-buttonsLine3[0].classes.push('button-dark');
-buttonsLine3[buttonsLine3.length-1].classes.push('button-double-size');
-buttonsLine3[buttonsLine3.length-1].classes.push('button-dark');
-
-let buttonsLine4 = returnButtonsLine(['Shift','z','x','c','v','b','n','m',',','.','/','\ArrowUp','Shift']); // \u9668 \u9650 \u9658 \u9660 вместо надписей
-buttonsLine4[0].classes.push('button-double-size');
-buttonsLine4[0].classes.push('button-dark');
-buttonsLine4[buttonsLine4.length-1].classes.push('button-double-size');
-buttonsLine4[buttonsLine4.length-1].classes.push('button-dark');
-
-
-const contentModel = {
-		type: 'main',
-		content: '',
-		classes: ['content'],
-		children: [
-			{
-				type: 'div',
-				content: '',
-				classes: ['container'],
-				children: [
-					{
-						type: 'div',
-						content: '',
-						classes: ['content__column'],
-						children: [
-							{
-								type: 'textarea',
-								content: '',
-								classes: ['content__inputarea'],
-								children: []
-							},
-							{
-								type: 'div',
-								content: '',
-								classes: ['content__keyboard-container'],
-								children: [
-									{
-										type: 'div',
-										content: '',
-										classes: ['content__keyboard-row'],
-										children: buttonsLine1
-									},
-									{
-										type: 'div',
-										content: '',
-										classes: ['content__keyboard-row'],
-										children: buttonsLine2
-									},
-									{
-										type: 'div',
-										content: '',
-										classes: ['content__keyboard-row'],
-										children: buttonsLine3
-									},
-									{
-										type: 'div',
-										content: '',
-										classes: ['content__keyboard-row'],
-										children: buttonsLine4
-									},
-									{
-										type: 'div',
-										content: '',
-										classes: ['content__keyboard-row'],
-										children: buttonsLine5
-									}												
-								]
-							}
-						]
-					}
-				]
-			}
-		]				
-	}
+          ],
+        },
+      ],
+    },
+  ],
+};
 
 const footerModel = {
   type: 'footer',
@@ -211,28 +226,28 @@ const footerModel = {
               type: 'h2',
               content: 'Клавиатура создана в операционной системе Windows',
               classes: ['footer__title'],
-              children: []
+              children: [],
             },
             {
               type: 'p',
               content: 'Комбинация для переключения языка: левые ctrl + alt',
               classes: ['footer__text'],
-              children: []
-            }
-          ]
-        }
-      ]
-    }
-  ]	
-}
+              children: [],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+};
 
 const appModel = [
-	{
+  {
     type: 'div',
     content: '',
     classes: ['wrapper'],
-    children: [headerModel, contentModel, footerModel]
-  }
-]
+    children: [headerModel, contentModel, footerModel],
+  },
+];
 
 export default appModel;
