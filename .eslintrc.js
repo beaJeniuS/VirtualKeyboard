25648642f15ae9762443d163bdb6418cd36f55d9
beaJenius <beajenius@yandex.ru>
2022-05-06 00:02:11 +0300

    feat: fix syntax problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,17 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "airbnb-base",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-  "rules": {
-    "no-var": "error"
-    "js": "always"
-    }
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-var': 'error',
+    'import/extensions': ['error', 'ignorePackages', {
+      js: 'always',
+    }],
+  },
+};
