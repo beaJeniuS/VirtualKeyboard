e0c2c52442a17456823fba839907cde4229df060
beaJenius <beajenius@yandex.ru>
2022-05-05 23:56:16 +0300

    feat: delete old test code, add creation through Block class

--- VKeyboard/js/app.js
+++ VKeyboard/js/app.js
@@ -1,34 +1,32 @@
-import keysEn from './keys_en.js'
-import keysRu from './keys_ru.js'
+import keysEn from './keys_en.js';
+import keysRu from './keys_ru.js';
+import Block from './block.js';
+import appModel from './appModel.js';
 
+const BODY = document.querySelector('body');
 export default class TaskApp {
   output;
+
   language = 'ru';
+
+  startBlock;
 
   constructor(title) {
     this.title = title;
   }
 
   init() {
-    const docBody = document.querySelector('body');
-    this.output = document.createElement('textarea');
-    this.output.setAttribute('id', 'output');
-    this.output.setAttribute('name', 'output-area');
-    this.output.setAttribute('cols', '100');
-    this.output.setAttribute('rows', '10');
-    docBody.prepend(this.output);
+    appModel.forEach((el) => {
+      this.startBlock = new Block(BODY, el);
+    });
+
     document.addEventListener('keydown', (event) => {
-      if (this.language === 'ru') {
-        if(event.shiftKey)
-          this.output.value += keysRu[event.code].shiftValue;
-        else
-          this.output.value += keysRu[event.code].value;        
-      } else {
-        if(event.shiftKey)
-          this.output.value += keysEn[event.code].shiftValue;
-        else
-          this.output.value += keysEn[event.code].value;
-      }
+      console.log(event.code);
+      /* if (this.language === 'ru') {
+         if (event.shiftKey) this.output.value += keysRu[event.code].shiftValue;
+         else this.output.value += keysRu[event.code].value;
+       } else if (event.shiftKey) this.output.value += keysEn[event.code].shiftValue;
+       else this.output.value += keysEn[event.code].value; */
     });
   }
 }
