ad840a8e303aa7a87f688e7f645979ed520abb54
beaJenius <beajenius@yandex.ru>
2022-05-05 23:59:27 +0300

    docs: delete comments

--- VKeyboard/index.js
+++ VKeyboard/index.js
@@ -2,13 +2,3 @@ import TaskApp from './js/app.js';
 
 const myApp = new TaskApp();
 myApp.init();
-// const log = document.getElementById('log');
-
-// const output = document.getElementById('output');
-
-/* function logKey(e) {
-  // output.innerText += `${e.code}`;
-  console.log(`${e.code}`);
-} */
-
-// document.addEventListener('keydown', logKey);
