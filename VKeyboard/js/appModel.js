8ad1c6f1b0ef71fb998abf693e4f1199728db02b
beaJenius <beajenius@yandex.ru>
2022-05-05 18:45:47 +0300

    init: add returnButtonsLine function for buttuns creating

--- VKeyboard/js/appModel.js
+++ VKeyboard/js/appModel.js
@@ -25,7 +25,74 @@ const headerModel = {
     }
   ]
 }
-  
+
+function returnButtonsLine(arr = []){
+		let resultArr = []
+		for(let i = 0;i < arr.length;i++) {
+			resultArr.push(
+			{
+				type: 'div',
+				content: '',
+				classes: ['content__button', 'button'],
+				styles: [],
+				action: true, //  pointer-events!!!!!!!!!!?????????
+				children: [
+					{
+						type: 'div',
+						content: '',
+						classes: ['button__lang-en'],
+						children: [
+							{
+								type: 'span',
+								content: arr[i], // взять из объекта с описанием кнопок
+								classes: ['button__value-normal'],
+								children: [],
+							},
+							{
+								type: 'span',
+								content: arr[i], // взять из объекта с описанием кнопок
+								classes: ['button__value-shift', 'hidden'],
+								children: []
+							},
+							{
+								type: 'span',
+								content: arr[i], // взять из объекта с описанием кнопок
+								classes: ['button__value-caps', 'hidden'],
+								children: []
+							},	
+						]
+					},					
+					{
+						type: 'div',
+						content: '',
+						classes: ['button__lang-ru', 'hidden'],
+						children: [
+							{
+								type: 'span',
+								content: arr[i], // взять из объекта с описанием кнопок
+								classes: ['button__value-normal', 'hidden'],
+								children: []
+							},
+							{
+								type: 'span',
+								content: arr[i], // взять из объекта с описанием кнопок
+								classes: ['button__value-shift', 'hidden'],
+								children: []
+							},
+							{
+								type: 'span',
+								content: arr[i], // взять из объекта с описанием кнопок
+								classes: ['button__value-caps', 'hidden'],
+								children: []
+							},	
+						]
+					}
+				]
+			});
+		}
+		return resultArr
+	}
+
 const appModel = [
 	{
     type: 'div',
